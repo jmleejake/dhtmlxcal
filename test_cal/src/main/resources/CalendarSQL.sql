f5d4997725b9e6879b12c8f217901b17d7f21868
kita <kita@kita-PC>
2017-03-24 11:32:49 +0900

    sql select추가후 커밋

--- test_cal/src/main/resources/CalendarSQL.sql
+++ test_cal/src/main/resources/CalendarSQL.sql
@@ -23,6 +23,13 @@ INSERT INTO EVENTS (ID, START_DATE, END_DATE, TEXT) VALUES ('4', TO_DATE('2017-0
   select * from events;
   select id, TO_CHAR(start_date, 'yyyy-mm-dd hh24:mi') start_date, TO_CHAR(end_date, 'yyyy-mm-dd hh24:mi') end_date, text from events where START_DATE like '__/03/__' or END_DATE like '__/03/__';
   
+  select 
+  		id
+  		, text
+  		, to_char(start_date, 'yyyy-mm-dd hh24:mi') start_date
+  		, to_char(end_date, 'yyyy-mm-dd hh24:mi') end_date
+  from events
+  
 --일정 저장하기
   INSERT INTO EVENTS (ID, START_DATE, END_DATE, TEXT) VALUES ('4', TO_DATE('2017-03-24 00:34', 'YYYY-MM-DD HH24:MI'), TO_DATE('2017-03-24 00:34:18', 'YYYY-MM-DD HH24:MI:SS'), '월세');
 
